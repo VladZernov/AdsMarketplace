900a016a183a58985c86a819ab50a6c849e65c6e
vlad <vlad.zernov@yahoo.com>
2016-01-30 02:10:08 +0200

    Categories(Domain) adding

---
+++ AdsMarketplace.Domain/Abstract/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using AdsMarketplace.Domain.Entities;
+using System.Collections.Generic;
+
+namespace AdsMarketplace.Domain.Abstract
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<TopCategory> TopCategories { get; }
+    }
+}
---
+++ AdsMarketplace.Domain/Concrete/EFCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using AdsMarketplace.Domain.Entities;
+using AdsMarketplace.Domain.Abstract;
+
+namespace AdsMarketplace.Domain.Concrete
+{
+    class EFCategoryRepository : ICategoryRepository
+    {
+        AdsContext context = new AdsContext();
+
+        public IEnumerable<TopCategory> TopCategories
+        {
+            get { return context.TopCategories; }
+        }
+    }
+}
